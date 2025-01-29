61d9700449289e66f04e4a598c76435715ed52c4
Rishi Sharma <116310789+AhamBrahmasmi0406@users.noreply.github.com>
2025-01-29 23:08:13 +0530

    Add files via upload
    Hospital project using JavaScript and Express

---
+++ index.js
@@ -0,0 +1,73 @@
+const express= require("express");
+
+const app= express();
+
+const users=[{
+    name:"John",
+    kidneys: [{
+        healthy: false
+    }]
+}];
+app.use(express.json());
+
+//Tells number of kidneys and their health
+app.get("/",function(req,res) {
+    const johnKidneys= users[0].kidneys;
+    const numberOfKidneys= johnKidneys.length;
+    let numberOfHealthyKidneys=0;
+
+    for(let i=0; i<johnKidneys.length; i++){
+        if(johnKidneys[i].healthy){
+            numberOfHealthyKidneys= numberOfHealthyKidneys + 1;   //first it assumes health kidney is 0 then iterates 
+
+        }
+    }
+ 
+   const numberOfUnhealthyKidneys= numberOfKidneys - numberOfHealthyKidneys;
+
+   res.json({
+    numberOfKidneys,
+    numberOfHealthyKidneys,             // returns the message in output
+    numberOfUnhealthyKidneys
+   })
+}) 
+
+//Adds new kidney, post requests
+app.post("/", function(req, res){
+   
+
+    const isHealthy= req.body.isHealthy;
+    users[0].kidneys.push({
+        healthy: isHealthy
+    })
+    res.json({
+        msg:"Done!"
+    })
+})
+
+app.put("/",function(req ,res) {
+    for(let i=0; i<users[0].kidneys.length; i++){
+        users[0].kidneys[i].healthy= true;
+    }
+    res.json({});
+})
+
+app.delete("/",function(req,res){
+    const newKidneys=[];
+    for(let i=0; i<users[0].kidneys.length;i++){
+        if(users[0].kidneys[i].healthy){
+            newKidneys.push({
+                healthy:true
+
+            })
+        }
+    }
+    users[0].kidneys= newKidneys;
+    res.json({msg:"done"})
+})
+
+
+
+
+
+app.listen(3000);
